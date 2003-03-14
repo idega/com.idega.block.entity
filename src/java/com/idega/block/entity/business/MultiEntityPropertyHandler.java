d92c6add70d14d60461e00e7cedfe2d23c28d731
Thomas Hilbig <thomas@idega.com>
2003-03-14 19:41:36 +0000

    optimization: search tree for entity pathes is cached in session

--- src/java/com/idega/block/entity/business/MultiEntityPropertyHandler.java
+++ src/java/com/idega/block/entity/business/MultiEntityPropertyHandler.java
@@ -2,13 +2,16 @@ package com.idega.block.entity.business;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.SortedMap;
+import java.util.SortedSet;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 import com.idega.block.entity.data.EntityPath;
 import com.idega.idegaweb.IWUserContext;
@@ -23,7 +26,8 @@ public class MultiEntityPropertyHandler {
   
   private Class leadingEntityClass = null;
   
-  private HashMap entityClassesHandler = new HashMap();
+  // needs to be synchronized, do not use Hashtable since null values are used 
+  private Map entityClassesHandler = Collections.synchronizedMap(new HashMap());
   
   private SortedMap allEntityPathes = null;
   
@@ -58,6 +62,8 @@ public class MultiEntityPropertyHandler {
     if (entityClass == leadingEntityClass)
       return;
     // it does not matter if there was no such entry
+    // allEntityPathes is still valid! (it knows more than it needs to know)
+    allEntityPathes = null;
     entityClassesHandler.remove(entityClass);
   }
  
@@ -65,7 +71,13 @@ public class MultiEntityPropertyHandler {
     // use cached value if possible
     if (allEntityPathes != null)
       return allEntityPathes;
-
+    // try do get the value from session
+    SortedMap sortedMap = getCachedEntityPathesFromSession();
+    if (sortedMap != null)  {
+      allEntityPathes = sortedMap;
+      return allEntityPathes;
+    }
+    // okay...then calculate again...
     allEntityPathes = new TreeMap(); 
     Set entries = entityClassesHandler.entrySet();
     Iterator iterator = entries.iterator();
@@ -78,8 +90,39 @@ public class MultiEntityPropertyHandler {
       }
       allEntityPathes.putAll(handler.getAllEntityPathes());
     }
+    // store in session
+    setCachedEntityPathesIntoSession();
     return allEntityPathes;
      
+  }
+  
+  private String getKeyForCachedEntityPathes() {
+    Set set = entityClassesHandler.keySet();
+    // we have to sort entries in order to get a unique key
+    SortedSet sortedSet = new TreeSet();
+    Iterator iterator = set.iterator();
+    // get unique name
+    while (iterator.hasNext())  {
+      String className = ((Class) iterator.next()).getName();
+      sortedSet.add(className);
+    }
+    StringBuffer buffer = new StringBuffer();
+    iterator = sortedSet.iterator();
+    while (iterator.hasNext())  {
+      buffer.append((String) iterator.next());
+      buffer.append(":");
+    }
+    return buffer.toString();
+  }
+  
+  private SortedMap getCachedEntityPathesFromSession() {
+    String key = getKeyForCachedEntityPathes();
+    return (SortedMap) userContext.getSessionAttribute(key);
+  }
+    
+  private void setCachedEntityPathesIntoSession() {
+    String key = getKeyForCachedEntityPathes();
+    userContext.setSessionAttribute(key, allEntityPathes);
   }
   
   
