e4b6de46455c10a9d83dfa35b540dec6fa37a6a7
Thomas Hilbig <thomas@idega.com>
2003-04-07 12:15:59 +0000

    EntityToPresentationObjectConverter interface has changed

--- src/java/com/idega/block/entity/business/EntityToPresentationObjectConverter.java
+++ src/java/com/idega/block/entity/business/EntityToPresentationObjectConverter.java
@@ -8,8 +8,24 @@ import com.idega.presentation.PresentationObject;
 /**
  *@author     <a href="mailto:thomas@idega.is">Thomas Hilbig</a>
  *@version    1.0
+ *
+ * This interface is used to define own converters for the EntityBrowser.
+ * The EntityBrowser provides a default converter, that is used if 
+ * a special converter was not defined. 
+ * That inner class is a good example how to implement this interface.
+ *
  */
 public interface EntityToPresentationObjectConverter {
+  
+  /** If you don't want to implement this method, just use the following line:
+   * return browser.getDefaultConverter().getHeadergetHeaderPresentationObject(entityPath, browser, iwc);
+   * 
+   * @param entityPath
+   * @param browser
+   * @param iwc
+   * @return
+   */
+  public PresentationObject getHeaderPresentationObject(EntityPath entityPath, EntityBrowser browser, IWContext iwc);
   
   public PresentationObject getPresentationObject(Object value, EntityPath path, EntityBrowser browser, IWContext iwc);
 }
