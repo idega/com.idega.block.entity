992b4b40d37d6d474d1c5850d18cfab05dc9808b
Thomas Hilbig <thomas@idega.com>
2003-07-16 14:48:06 +0000

    an implementation of EntityRepresantation

---
+++ src/java/com/idega/block/entity/data/EntityValueHolder.java
@@ -0,0 +1,45 @@
+package com.idega.block.entity.data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.idega.data.EntityRepresentation;
+
+/**
+ * <p>Title: idegaWeb</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: idega Software</p>
+ * @author <a href="thomas@idega.is">Thomas Hilbig</a>
+ * @version 1.0
+ * Created on Jul 15, 2003
+ */
+public class EntityValueHolder implements EntityRepresentation {
+  
+  private static final String DEFAULT_VALUE = "";
+  
+  private Map columnValueMap = null; 
+  
+  private Integer primaryKey = new Integer(-1);
+
+  /* (non-Javadoc)
+   * @see com.idega.data.EntityRepresentation#getColumnValue(java.lang.String)
+   */
+  public Object getColumnValue(String columnName) {
+    Object value = (columnValueMap == null) ? null : columnValueMap.get(columnName); 
+    return (value == null) ? DEFAULT_VALUE : value;
+  }
+  
+  public void setColumnValue(String columnName, Object value) {
+    if (columnValueMap == null)   {
+      columnValueMap = new HashMap();
+    }
+    columnValueMap.put(columnName, value);
+  }
+    
+  
+  public Object getPrimaryKey() {
+    return primaryKey;
+  }
+
+}
