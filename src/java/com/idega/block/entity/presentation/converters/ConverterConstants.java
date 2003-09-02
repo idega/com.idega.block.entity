4108e66f8638c1ec48ac4b5187b9660256f870e1
Thomas Hilbig <thomas@idega.com>
2003-09-02 19:13:16 +0000

    new constant added

--- src/java/com/idega/block/entity/presentation/converters/ConverterConstants.java
+++ src/java/com/idega/block/entity/presentation/converters/ConverterConstants.java
@@ -14,4 +14,6 @@ public interface ConverterConstants {
   public static String EDIT_ENTITY_KEY = "edit_entity";
 
   public static String EDIT_ENTITY_SUBMIT_KEY = "submit_edit_entity";
+  
+  public static Integer NEW_ENTITY_ID = new Integer(-1);
 }
